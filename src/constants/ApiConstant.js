208008d84e4d7228dd025b58f18f9f5f19ccd308
taras131 <mossnabitkana@gmail.com>
2024-02-14 18:32:21 +0300

    refactor_dependeses

--- src/constants/ApiConstant.js
+++ src/constants/ApiConstant.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'https://your-api-url.com/'
+export const API_BASE_URL = 'https://hammer_systems_1/'
 export const API_GET_CLIENTS = 'https://jsonplaceholder.typicode.com/users'
 export const PLAN_COLLECTION_NAME = "plan";
